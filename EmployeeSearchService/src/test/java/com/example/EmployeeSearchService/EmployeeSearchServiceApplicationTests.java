8a022a3e4de7c1c8de36da0ff756770b8e7aea0a
Ravi Rahul <rrravirahul02@gmail.com>
2021-04-09 02:01:55 +0530

    Add files via upload

---
+++ EmployeeSearchService/src/test/java/com/example/EmployeeSearchService/EmployeeSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeSearchService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
